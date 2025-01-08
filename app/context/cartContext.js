07b22d8f8f9e5a5fcaa9e6d8757a62cd2a03334f
Fatima_Iqra <iqraf683@gmail.com>
2025-01-08 17:19:39 +0500

    add filter functionality

--- app/context/cartContext.js
+++ app/context/cartContext.js
@@ -1,59 +1,63 @@
-"use client"
-import { createContext, useEffect, useState } from "react";
+"use client";
+import { createContext, useState, useEffect } from "react";
+
 export const cartContext = createContext();
+
 export const Cartprovider = ({ children }) => {
+  const [selectedFilters, setSelectedFilters] = useState({
+    Category: [],
+    Price: [],
+    Color: [],
+    Size: [],
+    Brand: [],
+  });
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+  const [activeView, setActiveView] = useState("grid");
+  const [productGrid, setproductGrid] = useState("four");
+  const [sortOption, setSortOption] = useState("Price:LowtoHigh");
+  const [products, setProducts] = useState([]);
+  const [totalProducts, setTotalProducts] = useState(0);
 
-    const [isSearchOpen, setIsSearchOpen] = useState(false);
-    const [activeView, setActiveView] = useState("grid");
-    const [productGrid, setproductGrid] = useState("four");
-    const [sortOption, setSortOption] = useState("Price:LowtoHigh");
-    const [products, setProducts] = useState([]);
-    const [totalProducts, setTotalProducts] = useState(0);
-    const [selectedFilters, setSelectedFilters] = useState({
-        Category: [],
-        Price: [],
-        Color: [],
-        Size: [],
-        Brand: [],
-    });
+  useEffect(() => {
+    setTotalProducts(products.length);
+  }, [products]);
 
+  const fourGrid = () => {
+    setActiveView("grid");
+    setproductGrid("four");
+  };
+  const twoGrid = () => {
+    setActiveView("list");
+    setproductGrid("two");
+  };
+  const handleFilterChange = (filter, option) => {
+    setSelectedFilters((prev) => ({
+      ...prev,
+      [filter]: prev[filter].includes(option)
+        ? prev[filter].filter((item) => item !== option)
+        : [...prev[filter], option],
+    }));
+  };
 
-    useEffect(() => {
-        setTotalProducts(products.length);
-    }, [products]);
-
-    const fourGrid = () => {
-        setActiveView("grid")
-        setproductGrid("four")
-    }
-    const twoGrid = () => {
-        setActiveView("list")
-        setproductGrid("two")
-    }
-
-    const handleCheckboxChange = (filter, value) => {
-        alert(value)
-    };
-
-
-
-
-    return (
-        <cartContext.Provider value={{
-            isSearchOpen,
-            setIsSearchOpen,
-            fourGrid,
-            twoGrid,
-            activeView,
-            productGrid,
-            sortOption,
-            setSortOption,
-            products,
-            setProducts,
-            totalProducts,
-            handleCheckboxChange
-        }}>
-            {children}
-        </cartContext.Provider>
-    )
-}
+  return (
+    <cartContext.Provider
+      value={{
+        selectedFilters,
+        handleFilterChange,
+        totalProducts,
+        activeView,
+        setSortOption,
+        products,
+        setProducts,
+        sortOption,
+        isSearchOpen,
+        setIsSearchOpen,
+        fourGrid,
+        twoGrid,
+        productGrid,
+      }}
+    >
+      {children}
+    </cartContext.Provider>
+  );
+};
