d10828e8cc0c08f672b1d5ab13b8c462ff600d5e
Fatima_Iqra <iqraf683@gmail.com>
2025-01-10 21:04:27 +0500

    update

--- app/cpanel/dashboard/page.jsx
+++ app/cpanel/dashboard/page.jsx
@@ -1,9 +1,19 @@
+"use client"
 import React from 'react'
-
+import {CldUploadWidget} from 'next-cloudinary'
 const Dashboard = () => {
   return (
     <div className='admin'> 
       <h1>Dashboard</h1>
+      <CldUploadWidget uploadPreset="<Your Upload Preset>">
+  {({ open }) => {
+    return (
+      <button onClick={() => open()}>
+        Upload an Image
+      </button>
+    );
+  }}
+</CldUploadWidget>
     </div>
   )
 }
