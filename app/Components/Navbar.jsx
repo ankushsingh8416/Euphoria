e6c22801a4727952ee72f73beabfcd95867815ee
ankush <nkshrazz@gmail.com>
2025-01-18 22:40:17 +0530

    update navbar

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -18,12 +18,16 @@ export default function Navbar() {
 
       <header className="shadow-xl z-50 sticky top-0">
         {/* Main Header Section */}
-        <div className="flex items-center justify-between px-4 md:px-6 py-3">
+        <div className="flex items-center justify-between px-4 md:px-6 py-2">
           {/* Left Section */}
           <div className="flex items-center">
-            <Link href="#" className="hidden lg:block text-sm font-medium text-gray-700 hover:underline">
-              Language :
-            </Link>
+            <div className="logo hidden lg:block">
+              <Link href="/">
+                <Image src="/images/euphoria.webp" alt="Anita Dongre" width={80} height={64} className="object-cover" />
+              </Link>
+            </div>
+
+
             <button onClick={() => setIsMenuOpen(true)} className="block lg:hidden">
               <img
                 width="30"
@@ -34,22 +38,60 @@ export default function Navbar() {
             </button>
           </div>
 
-          <div className="logo">
-            {/* Logo */}
+          <div
+            className={`fixed top-0 left-0 bg-[#faf8f0] w-[320px] h-screen z-40 transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
+              } lg:static lg:translate-x-0 lg:flex lg:h-auto lg:w-auto`}
+          >
+            <button
+              onClick={() => setIsMenuOpen(false)}
+              className="absolute top-8 right-8 lg:hidden"
+            >
+              <Image src="/images/cancel-icon.webp" alt="Close" width={30} height={30} />
+            </button>
+            <nav className="flex flex-col lg:flex-row items-start lg:items-center lg:justify-center w-full space-y-4 lg:space-y-0 lg:space-x-8 py-12 lg:py-4 px-4 lg:px-0 text-gray-700">
+              {[
+                "Women",
+                "Men",
+                "Wedding",
+                "Jewelry",
+                "Accessories",
+                "Gifting",
+                "Sale",
+              ].map((item, index) => {
+                const href = `/${item.toLowerCase().replace(/\s+/g, "-")}`;
+                return (
+                  <Link
+                    key={index}
+                    href={href}
+                    className="relative group hover:text-[#B18E35] transition-all duration-300"
+                    onClick={() => setIsMenuOpen(false)}
+                  >
+                    {item}
+                    <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#B18E35] transition-all duration-300 group-hover:w-full"></span>
+                  </Link>
+                );
+              })}
+            </nav>
+
+          </div>
+
+
+          <div className="logo block lg:hidden ">
             <Link href="/">
               <Image src="/images/euphoria.webp" alt="Anita Dongre" width={80} height={64} className="object-cover" />
             </Link>
           </div>
 
+
           {/* Right Section (Icons) */}
           <div className="flex items-center space-x-2 lg:space-x-4">
             {/* Search Icon */}
             <Link href="/search">
-            <Image 
-              src="/images/search-icon.svg" alt="Search" width={24} height={24} className="cursor-pointer"
-            />
+              <Image
+                src="/images/search-icon.svg" alt="Search" width={24} height={24} className="cursor-pointer"
+              />
             </Link>
-          
+
             {/* User Icon */}
             <Link href="/login">
               <Image src="/images/user-icon.svg" alt="User" width={24} height={24} className="cursor-pointer" />
@@ -64,43 +106,6 @@ export default function Navbar() {
           </div>
         </div>
 
-        <div
-          className={`fixed top-0 left-0 bg-[#faf8f0] w-[320px] h-screen z-40 transition-transform duration-300 ${isMenuOpen ? "translate-x-0" : "-translate-x-full"
-            } lg:static lg:translate-x-0 lg:flex lg:h-auto lg:w-auto`}
-        >
-          <button
-            onClick={() => setIsMenuOpen(false)}
-            className="absolute top-8 right-8 lg:hidden"
-          >
-            <Image src="/images/cancel-icon.webp" alt="Close" width={30} height={30} />
-          </button>
-          <nav className="flex flex-col lg:flex-row items-start lg:items-center lg:justify-center w-full space-y-4 lg:space-y-0 lg:space-x-8 py-12 lg:py-4 px-4 lg:px-0 text-gray-700">
-            {[
-              "Women",
-              "Men",
-              "Wedding",
-              "Jewelry",
-              "Accessories",
-              "Gifting",
-              "Sale",
-            ].map((item, index) => {
-              const href = `/${item.toLowerCase().replace(/\s+/g, "-")}`;
-
-              return (
-                <Link
-                  key={index}
-                  href={href}
-                  className="relative group hover:text-[#B18E35] transition-all duration-300"
-                  onClick={() => setIsMenuOpen(false)}
-                >
-                  {item}
-                  <span className="absolute left-0 -bottom-1 w-0 h-[2px] bg-[#B18E35] transition-all duration-300 group-hover:w-full"></span>
-                </Link>
-              );
-            })}
-          </nav>
-
-        </div>
       </header>
     </>
   );
