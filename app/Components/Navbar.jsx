33a7bb7201a2d4a10750cebbf472ff92438b9588
ankush <nkshrazz@gmail.com>
2025-01-18 12:32:44 +0530

    update new search

--- app/Components/Navbar.jsx
+++ app/Components/Navbar.jsx
@@ -1,8 +1,11 @@
+"use client"
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 
 
 export default function Navbar() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   return (
     <>
