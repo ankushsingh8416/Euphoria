966c3323b9b6fa090a11c706b5bbb116082c1fcf
Ankush Rajput <nkshrazz@gmail.com>
2025-02-21 21:16:26 +0530

    update img

--- app/layout.js
+++ app/layout.js
@@ -7,11 +7,14 @@ import { usePathname } from "next/navigation";
 import "./globals.css";
 import { Toaster } from "react-hot-toast";
 import SessionProviderWrapper from "./Components/SessionProviderWrapper";
+import Link from "next/link";
+import { RiChatSmile3Line, RiChatSmileAiLine } from "react-icons/ri";
+import { motion } from "framer-motion";
 
 export default function RootLayout({ children }) {
   const pathname = usePathname();
 
-  const noHeaderFooterRoutes = ["/cpanel/", "/search","/support"];
+  const noHeaderFooterRoutes = ["/cpanel/", "/search", "/support"];
 
   const isNoHeaderFooterRoute = noHeaderFooterRoutes.some((route) =>
     pathname.startsWith(route)
@@ -21,11 +24,38 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
         <SessionProviderWrapper>
-
           <Cartprovider>
             <Toaster />
             {!isNoHeaderFooterRoute && <Navbar />}
             {children}
+            <motion.div
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 0.4, ease: "easeOut" }}
+            >
+              <Link
+                href="/support"
+                className="fixed bottom-6 right-6 md:bottom-4 md:right-10 w-14 h-14 md:w-16 md:h-16 z-50 flex items-center justify-center rounded-full bg-[#1E381E] text-white "
+                >
+                <motion.div
+                  animate={{
+                    y: ["0%", "-8%", "0%"],
+                    filter: [
+                      "drop-shadow(0 0 5px rgba(34,197,94,0.6))",
+                      "drop-shadow(0 0 15px rgba(34,197,94,1))",
+                      "drop-shadow(0 0 5px rgba(34,197,94,0.6))",
+                    ],
+                  }}
+                  transition={{
+                    repeat: Infinity,
+                    duration:[ 0.8], // Fast → Slow → Fast effect
+                    ease: "easeInOut",
+                  }}
+                >
+                  <RiChatSmile3Line size={30} />
+                </motion.div>
+              </Link>
+            </motion.div>
             {!isNoHeaderFooterRoute && <Footer />}
           </Cartprovider>
         </SessionProviderWrapper>
--- app/page.js
+++ app/page.js
@@ -22,6 +22,7 @@ export default function Home() {
       <Gifting />
       <Celebrate />
       <FooterBanner />
+      
     </>
   );
 }
