b51b7aafd659d2467660ac0e719fe8b55912191a
Ankush Rajput <nkshrazz@gmail.com>
2025-03-03 23:14:06 +0530

    update share product

--- app/cpanel/customer/page.jsx
+++ app/cpanel/customer/page.jsx
@@ -1,27 +1,37 @@
+// import React from 'react'
+
+// const Customer = () => {
+//   const [users, setusers] = useState([])
+//   useEffect(() => {
+//     const fetchuser = async () => {
+//       const response = await fetch('/api/users')
+//       const data = await response.json()
+//       setusers(data)
+//     }
+//     fetchuser()
+
+
+//   }, [])
+
+
+
+//   return (
+//     <div>
+
+
+
+
+//     </div>
+//   )
+// }
+
+// export default page
+
 import React from 'react'
 
-const Customer = () => {
-  const [users, setusers] = useState([])
-  useEffect(() => {
-    const fetchuser = async () => {
-      const response = await fetch('/api/users')
-      const data = await response.json()
-      setusers(data)
-    }
-    fetchuser()
-
-
-  }, [])
-
-
-
+const page = () => {
   return (
-    <div>
-
-
-
-
-    </div>
+    <div>page</div>
   )
 }
 
